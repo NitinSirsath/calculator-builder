4d88e79c90a104f8364641da30ef21c64c934e20
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-02-13 14:16:17 +0530

    changed style for navbar

--- src/components/layout/header/ResponsiveAppBar.tsx
+++ src/components/layout/header/ResponsiveAppBar.tsx
@@ -50,7 +50,11 @@ function ResponsiveAppBar() {
   };
   return (
     <AppBar position="static" sx={{ borderRadius: 0 }}>
-      <Container maxWidth="xl">
+      <Container
+        sx={{
+          minWidth: "100%",
+        }}
+      >
         <Toolbar
           sx={{ display: "flex", justifyContent: "space-between" }}
           disableGutters
@@ -64,51 +68,6 @@ function ResponsiveAppBar() {
             />
           </Box>
 
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box> */}
           <Stack direction={"row"}>
             <Box mr={2}>
               <Tooltip
