aade813614774ebbcfa7219efaddfab4d7df0cfc
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-02-13 15:16:43 +0530

    update mobile view compatiblity

--- src/routes/HomePage/Index.tsx
+++ src/routes/HomePage/Index.tsx
@@ -1,11 +1,19 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 import Calculator from "./components/Calculator";
 import Sidebar from "./components/Sidebar";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    setIsTouchDevice("ontouchstart" in window || navigator.maxTouchPoints > 0);
+  }, []);
+
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
       <div className="flex flex-col md:flex-row gap-6 p-6 items-center justify-center">
         <Sidebar />
         <Calculator />
